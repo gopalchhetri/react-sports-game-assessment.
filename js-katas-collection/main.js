1b1c871675a28c619ec71061355ea6e13cc77ea7
gopal chhetri <gopalchhetri989@gmail.com>
2021-01-26 19:15:52 -0800

    file added

---
+++ js-katas-collection/main.js
@@ -0,0 +1,20 @@
+console.log("1: sum++")
+    function sum(a,b){
+        return a + b;
+}
+console.log(sum(5,9));
+console.log(sum(5,4));
+
+console.log("2: avg++");
+function avg(a,b,c){
+   let result = (a + b + c)/3
+    return result;
+}
+    console.log(avg(1,5,9));
+    console.log(avg(4,4,4));
+    console.log(avg(10,4,1));
+
+
+
+
+    
